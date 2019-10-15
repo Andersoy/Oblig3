4aeac2e3a1ea5f656d775fa9fe7936e284538bfb
Anders <andersoyrehagen@hotmail.com>
2019-10-15 17:16:42 +0200

    Oppgave 0-3.

--- src/no/oslomet/cs/algdat/Oblig3/ObligSBinTre.java
+++ src/no/oslomet/cs/algdat/Oblig3/ObligSBinTre.java
@@ -2,7 +2,20 @@ package no.oslomet.cs.algdat.Oblig3;
 
 ////////////////// ObligSBinTre /////////////////////////////////
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.*;
+
+
+
+/*
+Gruppemedlemmer:    S331398 - Anders Oeyrehagen
+                    S330474 - Anders Magnus Ytterboee
+                    S330473 - Tommy Grut
+                    S331386 - Tobias Dyre Evju
+                    S331359 - Theodor Fredheim Aandal
+
+ */
 
 public class ObligSBinTre<T> implements Beholder<T>
 {
@@ -44,9 +57,42 @@ public class ObligSBinTre<T> implements Beholder<T>
   }
   
   @Override
-  public boolean leggInn(T verdi)
-  {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+  public boolean leggInn(T verdi) {
+
+    Objects.requireNonNull(verdi);
+
+
+    Node<T> p = rot;    // p starter i roten
+    Node<T> q = null;   // hjelpevariabel
+    int cmp = 0;
+
+    while (p != null){       // fortsetter til p er ute av treet
+
+      q = p;                                 // q er forelder til p
+      cmp = comp.compare(verdi, p.verdi);    // bruker komparatoren
+      p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
+
+    }
+
+
+    // p er nå null, dvs. ute av treet, q er den siste vi passerte
+
+    p = new Node<>(verdi, q);                   // oppretter en ny node
+
+    if (q == null) {
+      rot = p;                  // p blir rotnode
+      rot.forelder = null;
+    }
+    else if (cmp < 0){
+      q.venstre = p;         // venstre barn til q
+    }
+    else{
+      q.høyre = p;                        // høyre barn til q
+    }
+
+    antall++;                                // én verdi mer i treet
+    return true;                             // vellykket innlegging
+
   }
   
   @Override
@@ -84,9 +130,36 @@ public class ObligSBinTre<T> implements Beholder<T>
     return antall;
   }
   
-  public int antall(T verdi)
-  {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+  public int antall(T verdi) {
+
+    if (verdi == null) {
+      return 0;
+    }
+
+    int antallAvVerdi = 0;
+    Node node = rot;
+
+
+    java.util.Deque<Node> fifo_queue = new java.util.ArrayDeque<>();
+    fifo_queue.addFirst(node);
+
+    while(fifo_queue.size() > 0) {
+      Node current = fifo_queue.removeLast();
+
+      if(current.verdi == verdi){
+        antallAvVerdi++;
+      }
+
+      if (current.venstre != null) {
+        fifo_queue.addFirst(current.venstre);
+      }
+      if (current.høyre != null) {
+        fifo_queue.addFirst(current.høyre);
+      }
+    }
+
+    return antallAvVerdi;
+
   }
   
   @Override
@@ -101,15 +174,56 @@ public class ObligSBinTre<T> implements Beholder<T>
     throw new UnsupportedOperationException("Ikke kodet ennå!");
   }
   
-  private static <T> Node<T> nesteInorden(Node<T> p)
-  {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
-  }
+  private static <T> Node<T> nesteInorden(Node<T> p) {
+
+
+
+    if (p.høyre != null)           // p har høyre subtre
+    {
+      p = p.høyre;
+
+      while(p.venstre != null){
+          p=p.venstre;
+      }
+    }
+
+    else                           // p har ikke høyre subtre
+    {
+      while (p.forelder != null && p == p.forelder.høyre)
+      {
+        p = p.forelder;            // fortsetter opp mot venstre
+      }
+      p = p.forelder;              // nå er p den neste (eller null)
+    }
+    return p;                  // returnerer verdien
+    }
+
   
   @Override
-  public String toString()
-  {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+  public String toString() {
+
+    if(tom()){
+
+      return "[]";
+    }
+
+    Node p = rot;
+    while(p.venstre != null){
+      p = p.venstre;
+    }
+
+    StringBuilder liste = new StringBuilder();
+    liste.append("[");
+    liste.append(p.verdi);
+    p = nesteInorden(p);
+    while(p != null){
+      liste.append(", " + p.verdi);
+      p = nesteInorden(p);
+    }
+
+    liste.append("]");
+    return liste.toString();
+
   }
   
   public String omvendtString()
